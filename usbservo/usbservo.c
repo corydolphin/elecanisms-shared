17d988dcffc4a561fde9570a03062ecd98cdbd8d
wcdolphin <wcdolphin@gmail.com>
2013-09-30 14:07:08 -0400

    Creates 40k signal

--- usbservo/usbservo.c
+++ usbservo/usbservo.c
@@ -14,6 +14,10 @@
 #define GET_VALS    2   // Vendor request that returns 2 unsigned integer values
 #define PRINT_VALS  3   // Vendor request that prints 2 unsigned integer values 
 
+
+#define WAITING 0 
+#define SENDING 1
+
 uint16_t val1, val2;
 
 void init(void){
@@ -23,13 +27,16 @@ void init(void){
     init_pin();
     init_oc();
 
-    timer_setPeriod(&timer3, 0.95);
-    timer_start(&timer3);
+    timer_setPeriod(&timer4, 0.95);
+    timer_start(&timer4);
 
     pin_digitalOut(&D[0]);
     pin_digitalOut(&D[1]);
+    pin_digitalOut(&D[2]);
     oc_servo(&oc1, &D[0], &timer1, 20E-3, 6E-4, 2.2E-3, 0);
     oc_servo(&oc2, &D[1], &timer2, 20E-3, 6E-4, 2.2E-3, 0);
+
+    oc_pwm(&oc3, &D[2], NULL, 40E3, 32000);
 
     led_on(&led1);
     val1 = 5;
@@ -42,18 +49,40 @@ void init(void){
 int16_t main(void) {
     init();
               
+    uint16_t duty, state;
+    duty = 0;
+    state = WAITING;
     while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
         ServiceUSB();                       // ...service USB requests
     }
-
     while (1) {
         ServiceUSB();                       // service any pending USB requests
         pin_write(&D[0], val1);
         pin_write(&D[1], val2);
+        // pin_write(&D[2], 0.5);
 
-        if (timer_flag(&timer3)) {
-            timer_lower(&timer3);
-            led_toggle(&led1);
+        if (timer_flag(&timer4)) {
+            timer_lower(&timer4);
+
+            switch(state){
+                case WAITING:
+                    led_on(&led1);
+                    led_off(&led2);
+                    state = SENDING;
+                    oc_pwm(&oc3, &D[3], NULL, 40E3, 32000);
+                    timer_setPeriod(&timer4, 500E-6);
+                    timer_start(&timer4);
+                    break;
+
+                case SENDING:
+                    led_on(&led2);
+                    led_off(&led1);
+                    state = WAITING;
+                    oc_pwm(&oc3, &D[3], NULL, 40E3, 0);
+                    timer_setPeriod(&timer4, 0.05);
+                    timer_start(&timer4);
+                    break;
+            }
         }
     }
 }
@@ -74,7 +103,6 @@ void VendorRequests(void) {
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
             break;
         case GET_VALS:
-            led_on(&led2);
             temp.w = val1;
             BD[EP0IN].address[0] = temp.b[0];
             BD[EP0IN].address[1] = temp.b[1];
